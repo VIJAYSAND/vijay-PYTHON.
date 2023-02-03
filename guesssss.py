d71ec29d4ed76c6059e294116f79f879604c72af
VIJJAY SANDEPAMU <103832271+VIJAYSAND@users.noreply.github.com>
2023-02-02 21:51:28 -0800

    Add files via upload

---
+++ guesssss.py
@@ -0,0 +1,14 @@
+import random
+n=random.randrange(1,1000)
+guess=int(input("enter any number:"))
+while n!=guess:
+    if guess<n:
+        print("too low")
+        guess=int(input("enter number again:"))
+    elif guess>n:
+        print("too high!")
+        guess=int(input("enter number again:"))
+    else:
+        break
+print("you guessed it right")
+        
